e74faac3ab78a464905d04da66a12090888f747a
GhulamMujtaba-jpg <mujtaba6065@gmail.com>
2026-02-25 06:02:15 +0500

    Update docs.php

--- docs.php
+++ docs.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login Page</title>
-</head>
-<body>
-
-<h2>Login</h2>
-
-<input type="email" id="email" placeholder="Enter Email"><br><br>
-<input type="password" id="password" placeholder="Enter Password"><br><br>
-
-<button onclick="login()">Login</button>
-
-<script>
-function login(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    // Login credentials
-    if(email === "admin@gmail.com" && password === "1234"){
-        window.location.href = "dashboard.html"; // Redirect to dashboard
-    } else {
-        alert("Wrong Email or Password");
-    }
+<?php
+session_start();
+if(!isset($_SESSION['phone'])){
+    header("Location: index.php");
+    exit();
 }
-</script>
-
-</body>
-</html>
+?>
